c00d5d78cb910ac3d522742e71f925c06646c0c4
dodik <leo.dodik@chamaeleon.de>
2025-06-10 11:04:30 +0200

    Add all initial files

---
+++ frontend/balkingo-frontend/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  showMenu = false;
+
+  constructor(private router: Router) {}
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  editProfile() {
+    this.router.navigate(['/profile-setup']);
+  }
+
+  logout() {
+    localStorage.removeItem('userEmail');
+    this.router.navigate(['/login']);
+  }
+}
