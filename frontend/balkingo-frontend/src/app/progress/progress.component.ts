59694798c7c410776a058a498dbb055289779955
dodik <leo.dodik@chamaeleon.de>
2025-07-08 10:05:57 +0200

    {#2} issue fix: fixed the breakpoint and responsivness error

--- frontend/balkingo-frontend/src/app/progress/progress.component.ts
+++ frontend/balkingo-frontend/src/app/progress/progress.component.ts
@@ -14,7 +14,7 @@ export class ProgressComponent implements OnInit {
   mobileMenuOpen = false;
   screenWidth = window.innerWidth;
 
-  lections = ['upoznavanje', 'brojevi', 'dani u tjednu','vrijeme','mjeseci/godisnja doba'];
+  lections = ['upoznavanje', 'brojevi', 'dani u tjednu','vrijeme','mjeseci'];
   completedLections: string[] = [];
 
   constructor(private router: Router) {}
